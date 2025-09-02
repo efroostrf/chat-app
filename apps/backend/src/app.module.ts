469a63bda2c41ade6e87ff054f7730ba7cc73ca6
Yefrosynii Kolenko <contact@yefro.dev>
2025-09-02 17:56:30 +0300

    feat: cqrs package

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [],
+  imports: [
+    // Using CQRS pattern
+    CqrsModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
