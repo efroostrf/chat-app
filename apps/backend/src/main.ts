08ac0bb30055fba98ddd1b88131268d5e04dd22d
Yefrosynii Kolenko <contact@yefro.dev>
2025-09-02 18:24:44 +0300

    feat: setup swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import fastifyCookie from '@fastify/cookie';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,6 +15,15 @@ async function bootstrap() {
   );
 
   await app.register(fastifyCookie);
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat App')
+    .setDescription('Chat App API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
